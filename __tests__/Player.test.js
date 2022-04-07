8c6dea629e8dba4a7bef47804ead4c537fb34f71
jrj-sys <jamesjamel99@gmail.com>
2022-04-06 23:46:32 -0500

    Player() constructor finished with tests

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -66,4 +66,14 @@ test('adds a potion to inventory', () => {
 
     expect(player.inventory.length).toBeGreaterThan(oldCount);
 });
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+})
   
