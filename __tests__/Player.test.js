c60b4cab5871ee5c39f5a7393d7fb6243e43b726
jrj-sys <jamesjamel99@gmail.com>
2022-04-06 22:49:34 -0500

    object tests and methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -20,4 +20,50 @@ test('gets inventory from player or returns false', () => {
   
     expect(player.getInventory()).toEqual(false);
   });
+
+test('gets player health value', () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0
+    
+    expect(player.isAlive()).toBeFalsy();
+})
+
+test('subtracts from player health', () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+})
+
+test('gets player attack value', () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
   
