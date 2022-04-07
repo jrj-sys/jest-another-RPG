014977f3c71c2f3505a9dc3291109f75281b4db2
jrj-sys <jamesjamel99@gmail.com>
2022-04-07 02:45:16 -0500

    Game constructor and inquirer functionality

---
+++ __tests__/Game.test.js
@@ -0,0 +1,12 @@
+const Game = require('../lib/Game.js')
+
+test('creates a Game object', () => {
+    const game = new Game();
+    
+//     expect(game.roundNumber).toEqual(0);
+//     expect(game.isPlayerTurn).toBeFalsy();
+//     expect(game.enemies).toContain('test')
+//     expect(game.currentEnemy).toBe();
+//     expect(enemy.player).toBe();
+})
+
